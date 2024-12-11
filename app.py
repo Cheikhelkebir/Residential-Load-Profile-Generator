0a7603f66b1efe36fa269511d515d9b0d832125d
Luis Enrique Garcia Marrero <lgarciamarrero92@gmail.com>
2024-12-11 17:52:00 +0100

    Minor updates

--- app.py
+++ app.py
@@ -483,5 +483,5 @@ def index():
 
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port="8099")
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port="8099")
+    #app.run(host='0.0.0.0')
